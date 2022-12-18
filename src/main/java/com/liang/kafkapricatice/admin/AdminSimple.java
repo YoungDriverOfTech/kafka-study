581037b27102270c6f17e91f33477649aab79714
liangxiaole <xiaoleliang045@gmail.com>
2022-12-18 17:59:23 +0900

    create topic

--- src/main/java/com/liang/kafkapricatice/admin/AdminSimple.java
+++ src/main/java/com/liang/kafkapricatice/admin/AdminSimple.java
@@ -2,14 +2,43 @@ package com.liang.kafkapricatice.admin;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class AdminSimple {
 
+    public final static String TOPIC_NAME = "jz_topic";
+
+
     public static void main(String[] args) {
+
+        // 1. 创建kafka集群
+//        AdminClient adminClient = AdminSimple.adminClient();
+//        System.out.println("adminClient = " + adminClient);
+
+        // 2. 创建topic实例
+        createTopic();
+    }
+
+    /*
+    * 创建Topic实例
+    * */
+    public static void createTopic() {
         AdminClient adminClient = AdminSimple.adminClient();
-        System.out.println("adminClient = " + adminClient);
+
+        // 副本因子
+        short rs = 1;
+
+        // 第二个参数是分区数量，第三个是副本数量
+        NewTopic newTopic = new NewTopic(TOPIC_NAME, 1, rs);
+
+        // createTopics接受数组
+        CreateTopicsResult topics = adminClient.createTopics(List.of(newTopic));
+        System.out.println("topics = " + topics);
     }
 
     /*
