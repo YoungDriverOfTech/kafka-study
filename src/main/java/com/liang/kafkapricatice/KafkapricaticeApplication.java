08d141255d86a8c4b98d620648ff579c3eab3d7b
liangxiaole <xiaoleliang045@gmail.com>
2022-12-18 16:57:29 +0900

    update

---
+++ src/main/java/com/liang/kafkapricatice/KafkapricaticeApplication.java
@@ -0,0 +1,13 @@
+package com.liang.kafkapricatice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkapricaticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkapricaticeApplication.class, args);
+    }
+
+}
