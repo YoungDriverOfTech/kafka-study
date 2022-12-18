919a4021c98653bd0b24360d99ba9e6e8ca6b868
liangxiaole <xiaoleliang045@gmail.com>
2022-12-18 18:13:15 +0900

    list topic

--- src/main/java/com/liang/kafkapricatice/admin/AdminSimple.java
+++ src/main/java/com/liang/kafkapricatice/admin/AdminSimple.java
@@ -1,28 +1,46 @@
 package com.liang.kafkapricatice.admin;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 public class AdminSimple {
 
     public final static String TOPIC_NAME = "jz_topic";
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // 1. 创建kafka集群
 //        AdminClient adminClient = AdminSimple.adminClient();
 //        System.out.println("adminClient = " + adminClient);
 
         // 2. 创建topic实例
-        createTopic();
+//        createTopic();
+
+        // 3. 获取topic名字
+        topicLists();
     }
+
+    public static void topicLists() throws Exception {
+        AdminClient adminClient = adminClient();
+
+        // 把内部topic一起打印出来
+        ListTopicsOptions options = new ListTopicsOptions();
+        options.listInternal(true);
+
+        // 不加options，不打印内部topic
+//        ListTopicsResult listTopicsResult = adminClient.listTopics();
+
+        // 加上options，把内部topic也能打出来
+        ListTopicsResult listTopicsResult = adminClient.listTopics(options);
+
+        Set<String> names = listTopicsResult.names().get();
+        names.forEach(System.out::println);
+    }
+
 
     /*
     * 创建Topic实例
