c632529aa997cbcd16375e040c27d5a4f881fa7a
liangxiaole <xiaoleliang045@gmail.com>
2022-12-20 23:13:17 +0900

    controller

--- src/main/java/com/liang/kafkapricatice/questionnaire/controller/QuestionnaireController.java
+++ src/main/java/com/liang/kafkapricatice/questionnaire/controller/QuestionnaireController.java
@@ -1,11 +1,35 @@
 package com.liang.kafkapricatice.questionnaire.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.liang.kafkapricatice.questionnaire.common.BaseResponseVO;
+import com.liang.kafkapricatice.questionnaire.conf.TemplateProperties;
+import com.liang.kafkapricatice.questionnaire.service.QuestionnaireService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
 public class QuestionnaireController {
+
+    @Autowired
+    private TemplateProperties properties;
+
+    @Autowired
+    private QuestionnaireService service;
+
+    @RequestMapping(value = "/template", method = RequestMethod.GET)
+    public BaseResponseVO getTemplate() {
+        return BaseResponseVO.success();
+    }
+
+    @RequestMapping(value = "/template/result", method = RequestMethod.GET)
+    public BaseResponseVO templateStatistics(@RequestParam (value = "templateId", required = false) String templateId) {
+        return BaseResponseVO.success();
+    }
+
+    @RequestMapping(value = "/template/report", method = RequestMethod.POST)
+    public BaseResponseVO dataReported(@RequestBody String reportData) {
+        return BaseResponseVO.success();
+    }
 
 
 }
