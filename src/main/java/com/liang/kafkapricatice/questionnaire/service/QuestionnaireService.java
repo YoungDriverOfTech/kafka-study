b1ecf65cad947364aebadcfb09b43b7a85018355
liangxiaole <xiaoleliang045@gmail.com>
2022-12-20 23:07:32 +0900

    service

--- src/main/java/com/liang/kafkapricatice/questionnaire/service/QuestionnaireService.java
+++ src/main/java/com/liang/kafkapricatice/questionnaire/service/QuestionnaireService.java
@@ -1,4 +1,21 @@
 package com.liang.kafkapricatice.questionnaire.service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.liang.kafkapricatice.questionnaire.conf.TemplateProperties;
+
 public interface QuestionnaireService {
+    /*
+    * get template
+    * */
+    TemplateProperties.Template getTemplate();
+
+    /*
+    * report questionnaire
+    * */
+    void templateReported(JSONObject reportInfo);
+
+    /*
+    * get result
+    * */
+    JSONObject templateStatistics(String templateId);
 }
--- src/main/java/com/liang/kafkapricatice/questionnaire/service/QuestionnaireServiceImpl.java
+++ src/main/java/com/liang/kafkapricatice/questionnaire/service/QuestionnaireServiceImpl.java
@@ -1,7 +1,43 @@
 package com.liang.kafkapricatice.questionnaire.service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.liang.kafkapricatice.questionnaire.conf.TemplateProperties;
+import com.liang.kafkapricatice.questionnaire.utils.FileUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class QuestionnaireServiceImpl implements QuestionnaireService{
+
+    @Autowired
+    private TemplateProperties properties;
+
+    @Override
+    public TemplateProperties.Template getTemplate() {
+        List<TemplateProperties.Template> templates = properties.getTemplates();
+        Optional<TemplateProperties.Template> first = templates.stream().filter(TemplateProperties.Template::isActive).findFirst();
+        return first.orElse(null);
+    }
+
+    @Override
+    public void templateReported(JSONObject reportInfo) {
+        // kafka producer 把数据推送到kafka
+        log.info("templateReported: [{}]", reportInfo);
+    }
+
+    @Override
+    public JSONObject templateStatistics(String templateId) {
+        if (properties.getTemplateResultType() == 0) {
+            // 文件获取
+            return FileUtils.readFile2JsonObject(properties.getTemplateResultFilePath()).get();
+        } else {
+            // db ...
+        }
+        return null;
+    }
 }
