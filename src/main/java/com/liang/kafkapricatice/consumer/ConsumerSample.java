d7c4f9c463eda5ca3d8cadaf18dd54922ab0e6fb
liangxiaole <xiaoleliang045@gmail.com>
2022-12-22 22:30:27 +0900

    update

--- src/main/java/com/liang/kafkapricatice/consumer/ConsumerSample.java
+++ src/main/java/com/liang/kafkapricatice/consumer/ConsumerSample.java
@@ -16,10 +16,14 @@ public class ConsumerSample {
 
         // 1. 最简单的消费者
         helloWorld();
+
+        // 2. 手动提交
+        commitOffsetManually();
+
     }
 
     /*
-    * 最简单的消费者
+    * 最简单的消费者（工作中不推荐）
     * */
     private static void helloWorld() {
         Properties props = new Properties();
@@ -35,12 +39,41 @@ public class ConsumerSample {
         // 订阅哪个topic
         consumer.subscribe(List.of(TOPIC_NAME));
         while (true) {
-
             // 没间隔100毫秒去kafka拉取记录
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, String> record : records) {
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
             }
+        }
+    }
+
+    /*
+    * 手动提交offset
+    * */
+    private static void commitOffsetManually() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "ec2-35-73-156-207.ap-northeast-1.compute.amazonaws.com:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "false"); // 关闭自动提交
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        // 订阅哪个topic
+        consumer.subscribe(List.of(TOPIC_NAME));
+
+        while (true) {
+            // 没间隔100毫秒去kafka拉取记录
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                // 把数据存入数据库
+                // 假设这块已经存入了db
+
+                // 如果失败则回滚，不要提交offset
+            }
+
+            // 手动通知offSet提交
+            consumer.commitAsync();
         }
     }
 }
